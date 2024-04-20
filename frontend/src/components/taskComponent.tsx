8443d9fce07338fcf490fa4741cda580ff696458
roll1226 <tomo_yasu_4724@icloud.com>
2024-04-21 06:38:08 +0900

    add: insert method

--- frontend/src/components/taskComponent.tsx
+++ frontend/src/components/taskComponent.tsx
@@ -3,17 +3,19 @@ import {
   useRemoveTaskMutation,
 } from "@/gql/tasks/tasks.gen";
 import { useTaskSubscription } from "@/hooks/subscription/useTaskSubscription";
+import { useState } from "react";
 
 export const TaskComponent = () => {
   const [InsertTaskMutation] = useInsertTaskMutation();
   const [RemoveTaskMutation] = useRemoveTaskMutation();
-
   const { tasks, taskError, taskLoading } = useTaskSubscription();
+
+  const [taskName, setTaskName] = useState("");
 
   const addTask = () => {
     InsertTaskMutation({
       variables: {
-        name: "テスト5",
+        name: taskName,
         customer_id: 1,
       },
     });
@@ -29,9 +31,16 @@ export const TaskComponent = () => {
 
   return (
     <>
+      <input
+        type="text"
+        value={taskName}
+        onChange={(event) => setTaskName(event.target.value)}
+      />
+      <br />
       <button type="button" onClick={addTask}>
         追加
       </button>
+      <hr />
       {taskLoading ? (
         <p>Loading</p>
       ) : (
