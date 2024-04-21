9c548ededea3105f4e4182a0f6726aa7c831fc52
roll1226 <tomo_yasu_4724@icloud.com>
2024-04-22 06:32:49 +0900

    add: firebase

--- frontend/src/utils/lib/firebase/FirebaseInitialize.ts
+++ frontend/src/utils/lib/firebase/FirebaseInitialize.ts
@@ -3,6 +3,8 @@ import { env } from "@/env/dotEnv";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
 
 const isEmulator = () => {
   const useEmulator = env.getFirebaseEmulator();
@@ -15,11 +17,21 @@ const app = getApps().length
 
 const firebaseAuth = getAuth(app);
 const firebaseFunction = getFunctions(app);
+const firebaseFirestore = getFirestore(app);
+const firebaseStorage = getStorage(app);
 const analytics = getAnalytics(app);
 
 if (isEmulator()) {
   connectAuthEmulator(firebaseAuth, "http://localhost:9099");
-  connectFunctionsEmulator(firebaseFunction, "localhost", 5000);
+  connectFirestoreEmulator(firebaseFirestore, "localhost", 8000);
+  connectStorageEmulator(firebaseStorage, "localhost", 9199);
+  connectFunctionsEmulator(firebaseFunction, "localhost", 5001);
 }
 
-export { firebaseAuth, firebaseFunction, analytics };
+export {
+  firebaseAuth,
+  firebaseFunction,
+  firebaseFirestore,
+  firebaseStorage,
+  analytics,
+};
