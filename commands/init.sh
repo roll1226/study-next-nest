3edf39bb2a054c4d620af14bb554b6becfbe59d9
roll1226 <tomo_yasu_4724@icloud.com>
2024-04-25 08:13:27 +0900

    change: echo color

--- commands/init.sh
+++ commands/init.sh
@@ -1,6 +1,6 @@
 # Move to repository root
-cd "$(git rev-parse --show-toplevel)" && echo "Moved: Repository Root"
+cd "$(git rev-parse --show-toplevel)" && echo "\033[32mMoved:\033[m Repository Root"
 
-ln -sf "$(pwd)/.env" frontend/.env.test && echo 'Linked: frontend/.env'
-ln -sf "$(pwd)/.env" backend/.env.test && echo 'Linked: backend/.env'
-ln -sf "$(pwd)/.env" hasura/.env.test && echo 'Linked: hasura/.env'
+ln -sf "$(pwd)/.env" frontend/.env.test && echo "\033[32mLinked:\033[m frontend/.env"
+ln -sf "$(pwd)/.env" backend/.env.test && echo "\033[32mLinked:\033[m backend/.env"
+ln -sf "$(pwd)/.env" hasura/.env.test && echo "\033[32mLinked:\033[m hasura/.env"
