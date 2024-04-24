892122b45e47eafff39fb411febd340690a7335c
roll1226 <tomo_yasu_4724@icloud.com>
2024-04-25 08:16:34 +0900

    fix: init.sh

--- commands/init.sh
+++ commands/init.sh
@@ -1,6 +1,6 @@
 # Move to repository root
 cd "$(git rev-parse --show-toplevel)" && echo "\033[32mMoved:\033[m Repository Root"
 
-ln -sf "$(pwd)/.env" frontend/.env.test && echo "\033[32mLinked:\033[m frontend/.env"
-ln -sf "$(pwd)/.env" backend/.env.test && echo "\033[32mLinked:\033[m backend/.env"
-ln -sf "$(pwd)/.env" hasura/.env.test && echo "\033[32mLinked:\033[m hasura/.env"
+ln -sf "$(pwd)/.env" frontend/.env && echo "\033[32mLinked:\033[m frontend/.env"
+ln -sf "$(pwd)/.env" backend/.env && echo "\033[32mLinked:\033[m backend/.env"
+ln -sf "$(pwd)/.env" hasura/.env && echo "\033[32mLinked:\033[m hasura/.env"
