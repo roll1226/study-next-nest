536e894b9da284825efacc41386b24dc330626f8
roll1226 <tomo_yasu_4724@icloud.com>
2024-04-26 05:38:56 +0900

    add: comment

--- frontend/src/utils/lib/jwt/Jwt.ts
+++ frontend/src/utils/lib/jwt/Jwt.ts
@@ -1,6 +1,7 @@
 import { Logger } from "@/utils/debugger/Logger";
 import * as jwt from "jsonwebtoken";
 
+// FIrebase Functionsに移す予定
 export const Jwt = {
   getEmulatedSignedToken: (token: string) => {
     return jwt.sign(
